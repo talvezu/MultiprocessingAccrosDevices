05162209583c42d3e1d6dfabbefd9657f91229d8
talvezu <talvezu@walla.com>
2023-06-18 22:23:50 +0300

    docker, debug and making sure running on clean env

--- Docker/docker_env.sh
+++ Docker/docker_env.sh
@@ -2,4 +2,5 @@
 
 
 python3 generate_conf_files.py
+docker-compose -f docker_compose.yaml down --rmi all
 docker-compose -f docker_compose.yaml up -d
--- python/server.py
+++ python/server.py
@@ -70,4 +70,4 @@ def register_for_vector_generating_service():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
